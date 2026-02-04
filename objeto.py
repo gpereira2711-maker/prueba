3fc7399927313c775cbac6c4bf900c35c7ee7753
gpereira2711-maker <gpereira2711@gmail.com>
2026-02-04 17:18:25 -0400

    ultima prueba

---
+++ objeto.py
@@ -0,0 +1,9 @@
+class ciudad_Tokio:
+    def __init__(self, poblacion, clima, ruta):
+        self.poblacion = poblacion
+        self.clima = clima
+        self.ruta = ruta
+        
+ciudad_Tokio = (1500, 'calidad','facil')
+print(ciudad_Tokio)
+    
